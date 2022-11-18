24d48b8738ea8567d3a6c744dc02699e174f72b6
Marten (msc) <msc@odoo.com>
2022-11-18 15:07:13 -0500

    switch image output format to png

--- .eleventy.js
+++ .eleventy.js
@@ -12,7 +12,7 @@ const Image = require("@11ty/eleventy-img");
 async function imageShortcode(src, alt, sizes) {
   let metadata = await Image(src, {
     widths: [300, 600],
-    formats: ["avif", "jpeg"]
+    formats: ["png"]
   });
 
   let imageAttributes = {
